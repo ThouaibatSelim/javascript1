8e6c8bbbbdb2598a042e980457b04cb6d169662a
Espace NUMERIQUE 12 <espace12@numerique.lan>
2024-10-16 12:07:28 +0300

    ex film

--- exercice.js
+++ exercice.js
@@ -1,4 +1,5 @@
 
+// 11/10
 // // //nombres initial de livres
 // // let totalLivres = 500
 
@@ -15,21 +16,36 @@
 
 // // console.log (affichageTotalLivres, totalLivres)
 
-
+// 15/10
 let age = 14;
 
-//if user age is inferior to 13 
+// //if user age is inferior to 13 
 if (age < 13) {
-    console.log  ("Le Roi Lion");
- } 
+console.log  ("Le Roi Lion");
+} 
 
- // if user age is between 13&18
+//  // if user age is between 13&18
 if (age > 13 && age < 18) {
-    console.log ("Titanic");
+console.log ("Titanic");
 }
 
-// if user age is superior to 18
+//if user age is superior to 18
 if (age > 18) {
-    console.log ("Rambo 2");
+ console.log ("Rambo 2");
 }
 
+// 16/10
+
+let ticket = {
+    nomdeFilm: "Koungou",
+    prix: 6.90 ,  
+    numeroSalle: 4086,
+};
+
+let nom = "Thouaïbat";
+
+let texteAffichage = ("Bonjour", nom, "votre film", ticket.nomdeFilm, "est en salle", ticket.numeroSalle) ;
+
+console.log (texteAffichage);
+
+// console.log ("Bonjour", nom, "votre film", film.nomdeFilm, "est en salle", film.numeroSalle);
